61725889c799a8d72680d70b79db9d22c78554ca
gkavishika21 <47886738+gkavishika21@users.noreply.github.com>
2019-05-25 23:06:45 +0530

    Add files via upload

---
+++ productManager/src/main/java/net/codejava/ProductRepository.java
@@ -0,0 +1,7 @@
+package net.codejava;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface ProductRepository extends JpaRepositoryImplementation<Product, Long>{
+
+}
